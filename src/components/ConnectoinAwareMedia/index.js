37c4a4bbd67a3d12ee34b956516c754e48c48a8b
anton-karlovskiy <antonkarlovskiy@outlook.com>
2019-07-11 13:58:56 +0300

    updated with local glitch assets url

--- src/components/ConnectoinAwareMedia/index.js
+++ src/components/ConnectoinAwareMedia/index.js
@@ -14,19 +14,19 @@ const ConnectionAwareMedia = () => {
     // case 'offline':
     //   break;
     case 'slow-2g':
-      media = <img className="responsive" src="https://img.youtube.com/vi/DYYtuKyMtY8/mqdefault.jpg" alt="minimum size" />;
+      media = <img className="responsive" src="https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmin-res.jpg?v=1562842586912" alt="minimum size" />;
       break;
     case '2g':
-      media = <img className="responsive" src="https://img.youtube.com/vi/DYYtuKyMtY8/0.jpg" alt="medium size" />;
+      media = <img className="responsive" src="https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmedium-res.jpg?v=1562842587169" alt="medium size" />;
       break;
     case '3g':
-      media = <img className="responsive" src="https://img.youtube.com/vi/DYYtuKyMtY8/maxresdefault.jpg" alt="maximum size" />;
+      media = <img className="responsive" src="https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2Fmax-res.jpg?v=1562842587982" alt="maximum size" />;
       break;
     case '4g':
-      media = <video className="responsive" src="https://cdn.glitch.com/a5370f7a-8a0f-491c-8831-ff3dd5544def%2F4g-video.mp4?v=1562840860124" controls />;
+      media = <video className="responsive" src="https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2F4g-video.mp4?v=1562842601068" controls />;
       break;
     default:
-      media = <video className="responsive" src="https://cdn.glitch.com/a5370f7a-8a0f-491c-8831-ff3dd5544def%2F4g-video.mp4?v=1562840860124" controls />;
+      media = <video className="responsive" src="https://cdn.glitch.com/8d7fb7f0-a9be-4a8c-96c7-8af286af487e%2F4g-video.mp4?v=1562842601068" controls />;
       break;
   }
 
